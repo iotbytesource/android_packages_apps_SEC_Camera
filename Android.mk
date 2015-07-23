f422c0b966f4bdf2a78fd4704483a15dc89a4ea6
root <root@ubuntu.(none)>
2015-07-23 10:18:32 +0800

    init git repository

---
+++ Android.mk
@@ -0,0 +1,28 @@
+ifeq ($(USE_SEC_CAMERA),true)
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := SEC_Camera
+#LOCAL_SDK_VERSION := current
+
+LOCAL_JNI_SHARED_LIBRARIES := libjni_SECmosaic
+
+LOCAL_REQUIRED_MODULES := libjni_SECmosaic
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+#LOCAL_OVERRIDES_PACKAGES := Camera
+
+include $(BUILD_PACKAGE)
+
+ifeq ($(strip $(LOCAL_PACKAGE_OVERRIDES)),)
+# Use the following include to make our test apk.
+include $(call all-makefiles-under, $(LOCAL_PATH))
+endif
+
+endif
